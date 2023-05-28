059c572af5ce87794f6614d87ee6dbdf99ff96f2
Okashe <okashabdi88@gmail.com>
2023-05-28 17:00:03 -0500

    colum and row challenge completed

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,39 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            color: Colors.white,
-            margin: const EdgeInsets.only(left: 30.0),
-            padding: const EdgeInsets.all(20.0),
-            child: const Text('Hello'),
-          ),
-        ),
+            child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              width: 100.0,
+              color: Colors.red,
+              child: const Text('Container 1'),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.yellow,
+                  child: const Text('Container 2'),
+                ),
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.green,
+                  child: const Text('Container 2'),
+                ),
+              ],
+            ),
+            Container(
+              width: 100.0,
+              color: Colors.blue,
+              child: const Text('Container 3'),
+            ),
+          ],
+        )),
       ),
     ),
   );
